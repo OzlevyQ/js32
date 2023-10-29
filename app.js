ee4f94bbab333258e28a37af579005238520c7e9
OzlevyQ <108112915+OzlevyQ@users.noreply.github.com>
2023-10-29 20:01:04 +0200

    ├kior

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-
+app.use(express.static('dist'))
 app.use('/yadbarzel/feed.xml', createProxyMiddleware({ 
   target: 'https://media.rss.com', 
   changeOrigin: true 
 }));
 
-app.listen(3000);
+app.listen(8080);
