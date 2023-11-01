8872a5a13042eaca3cde31f1f525a34ec124435f
OzlevyQ <108112915+OzlevyQ@users.noreply.github.com>
2023-11-01 18:45:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 app.use(express.static('dist'))
+
 app.use('/yadbarzel/feed.xml', createProxyMiddleware({ 
   target: 'https://media.rss.com', 
   changeOrigin: true 
 }));
 
-app.listen(8080, () => {
-  console.log("Server is running and serving the site from 'dist' directory on port 8080");
-  console.log("Access the site at: http://localhost:8080");
-});
+app.listen(3000, () => {});
